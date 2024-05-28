334fe63a93659f8d36708bcd479e91d079c5ddbe
Anna Kobis <anna.kobis.97@yandex.ru>
2024-05-28 23:36:19 +0300

    добавлены скрипты

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Проектная работа Mesto
-
+https://github.com/anna-kobis/mesto-project-ff.git
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,28 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
+const cardList = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
+function createCard(dataCard, deleteCard) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+
+  cardElement.querySelector(".card__title").textContent = dataCard.name;
+  cardElement.querySelector(".card__image").src = dataCard.link;
+
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", deleteCard);
+
+  return cardElement;
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(evt) {
+  evt.target.closest(".card").remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(item => {
+  cardList.append(createCard(item, deleteCard));
+});
